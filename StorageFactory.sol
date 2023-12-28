e2396ddee8b42ca2160b49a3c800db6fe9c2d1e5
Prince Allwin <princeallwin199@gmail.com>
2023-12-28 19:31:31 +0530

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,25 +1,30 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity 0.8.18;
 
 import { SimpleStorage } from "./SimpleStorage.sol";
 
 contract StorageFactory{
     // This storage factory contract will deploy the SimpleStorage contract.
     // By importing SimpleStorage, we have access to all its contents.
+    // Smart contracts are "composable" because they can easily interact with each other
 
-    //type visibility variableName
+    //type -> visibility -> variableName
     SimpleStorage[] public listOfsimpleStorageContracts;
 
     function createSimpleStorageContract() public {
         SimpleStorage newSimpleStorage = new SimpleStorage();
         listOfsimpleStorageContracts.push(newSimpleStorage);
         // Create a new instance of the SimpleStorage contract
-        // Assign the newly created instance to the simpleStorage variable
+        // Assign the newly created instance to the newSimpleStorage variable
     }
 
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         SimpleStorage mySimpleStorage = listOfsimpleStorageContracts[_simpleStorageIndex];
         mySimpleStorage.store(_simpleStorageNumber);
+        // using sfstore we are storing the favNumber for specific contract
+        // To interact with other contracts we need 
+        // 1. Address of the contract
+        // 2. Application Binary Interface(ABI) of the contract 
     }
 
     function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
